351d6dd2005405ba397f0de5b6b40e69630f2437
Nitin Kushwaha <72150554+INitinKushwaha@users.noreply.github.com>
2024-06-13 23:06:02 +0530

    Update server.js

--- task-manger-server/src/server.js
+++ task-manger-server/src/server.js
@@ -10,7 +10,12 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors(
+    {
+        origin : ["assignment-project-omega.vercel.app"],
+        methods: ["POST","GET"]
+    }
+));
 app.use(express.json());
 
 // Routes
